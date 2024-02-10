791b5a6228129b670e8f7c86c5521758d2e9c286
Murenzi Paterne <murenzi419@gmail.com>
2024-02-10 22:57:56 +0200

    Revert "created boilerplates for controllers and models of articles, comments, user, tasks"
    This reverts commit 69864108c710f3b5ca1cd7824ffe9b69aa895917.

--- dist/controllers/UserController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserController {
-    login() { }
-    register() { }
-    update() { }
-}
-exports.default = new UserController();
