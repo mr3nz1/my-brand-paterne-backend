970255c4464f4569de70007674dba4e863f85675
Murenzi Paterne <murenzi419@gmail.com>
2024-02-10 23:03:02 +0200

    removed things from develop

--- dist/controllers/UserControllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserControllers = void 0;
-class UserControllers {
-    login() { }
-    register() { }
-    update() { }
-}
-exports.UserControllers = UserControllers;
