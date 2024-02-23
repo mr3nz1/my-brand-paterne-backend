bb274b3c847b6ed4dbf23a33e20dbd0179459f26
Murenzi Paterne <murenzi419@gmail.com>
2024-02-23 22:54:49 +0200

    articles tests

--- src/__tests__/ArticlesTests.ts
+++ src/__tests__/ArticlesTests.ts
@@ -1,12 +1,212 @@
-import app from "../app";
-import request from "supertest";
+// import app from "../app";
+// import request from "supertest";
+// import fs from "fs/promises";
+// import mongoose from "mongoose";
 
-describe("add article", () => {
+// let token: string;
+// let id: string;
 
-});
-describe("get articles", () => {
-    test("article ")
-});
-describe("get article", () => {});
-describe("edit article", () => {});
-describe("delete article", () => {});
+// beforeAll(async () => {
+//   const userLoginInfo = {
+//     email: "test@gmail.com",
+//     password: "123",
+//   };
+
+//   const response = await request(app)
+//     .post("/api/v1/users/login")
+//     .send(userLoginInfo);
+
+//   token = response.body.data.token;
+// });
+
+// describe("articles", () => {
+//   describe("add article", () => {
+//     it("Should add an article when data is provided", async () => {
+//       try {
+//         const bannerImage = await fs.readFile(
+//           `uploads/article_banner_image.jpg`
+//         );
+
+//         const response = await request(app)
+//           .post("/api/v1/articles")
+//           .set("Authorization", "Bearer " + token)
+//           .field("title", "Test title")
+//           .field("description", "Description for the test")
+//           .field("content", "Content for the test")
+//           .field("isPublished", true)
+//           .attach("bannerImage", bannerImage, "article_banner_image.jpg");
+
+//         expect(response.statusCode).toEqual(201);
+//         expect(typeof response.body.data).toEqual("object");
+//         expect(typeof response.body.data.article).toEqual("object");
+
+//         id = response.body.data.article.id;
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+
+//     it("Should throw an error when some data are missing", async () => {
+//       try {
+//         const bannerImage = await fs.readFile(
+//           `uploads/article_banner_image.jpg`
+//         );
+
+//         const response = await request(app)
+//           .post("/api/v1/articles")
+//           .set("Authorization", "Bearer " + token)
+//           .field("title", "Test title")
+//           .field("content", "Content for the test")
+//           .field("isPublished", true)
+//           .attach("bannerImage", bannerImage, "article_banner_image.jpg");
+
+//         expect(response.statusCode).toEqual(500);
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+
+//     it("Should throw an error if the token is not provided", async () => {
+//       try {
+//         const bannerImage = await fs.readFile(
+//           `uploads/article_banner_image.jpg`
+//         );
+
+//         const response = await request(app)
+//           .post("/api/v1/articles")
+//           .field("title", "Test title")
+//           .field("content", "Content for the test")
+//           .field("isPublished", true)
+//           .attach("bannerImage", bannerImage, "article_banner_image.jpg");
+
+//         expect(response.statusCode).toEqual(403);
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+//   });
+
+//   describe("update articles", () => {
+//     it("Should update the article when all data is provided", async () => {
+//       try {
+//         const bannerImage = await fs.readFile(
+//           `uploads/article_banner_image.jpg`
+//         );
+
+//         const response = await request(app)
+//           .patch("/api/v1/articles/" + id)
+//           .set("Authorization", "Bearer " + token)
+//           .attach("bannerImage", bannerImage, "article_banner_image.jpg");
+
+//         expect(response.statusCode).toEqual(200);
+//         expect(typeof response.body.data).toEqual("object");
+//         expect(typeof response.body.data.article).toEqual("object");
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+
+//     it("Should throw an error if the token is not provided", async () => {
+//       try {
+//         const bannerImage = await fs.readFile(
+//           `uploads/article_banner_image.jpg`
+//         );
+
+//         const response = await request(app)
+//           .patch("/api/v1/articles/" + id)
+//           .attach("bannerImage", bannerImage, "article_banner_image.jpg");
+
+//         expect(response.statusCode).toEqual(403);
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+//   });
+
+//   describe("get article", () => {
+//     it("Should return the article if it exists", async () => {
+//       try {
+//         const response = await request(app).get("/api/v1/articles/" + id);
+
+//         expect(response.statusCode).toEqual(200);
+//         expect(typeof response.body.data).toEqual("object");
+//         expect(typeof response.body.data.article).toEqual("object");
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+
+//     it("Bad id format", async () => {
+//       try {
+//         const response = await request(app).get(
+//           "/api/v1/articles/" + id + "asdfa"
+//         );
+
+//         expect(response.statusCode).toEqual(500);
+//         expect(typeof response.body.message).toEqual("string");
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+
+//     it("Should throw an error if there is no article with the given id", async () => {
+//       try {
+//         const response = await request(app).get(
+//           "/api/v1/articles/" + "65cf61377818ede2c9d0863a"
+//         );
+
+//         expect(response.statusCode).toEqual(404);
+//         expect(typeof response.body.message).toEqual("string");
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+//   });
+
+//   describe("get articles", () => {
+//     it("articles", async () => {
+//       try {
+//         const response = await request(app).get("/api/v1/articles/");
+
+//         expect(response.statusCode).toEqual(200);
+//         expect(typeof response.body.data).toEqual("object");
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+//   });
+
+//   describe("delete article", () => {
+//     it("Should delete the article if it exists", async () => {
+//       try {
+//         const response = await request(app)
+//           .delete("/api/v1/articles/" + id)
+//           .set("Authorization", "Bearer " + token);
+//         expect(response.statusCode).toEqual(200);
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+
+//     it("Should throw error if there is no article with given id", async () => {
+//       try {
+//         const response = await request(app)
+//           .get("/api/v1/articles/" + "65cf61377818ede2c9d0863a")
+//           .set("Authorization", "Bearer " + token);
+
+//         expect(response.statusCode).toEqual(404);
+//         expect(typeof response.body.message).toEqual("string");
+//       } catch (err) {
+//         throw err;
+//       }
+//     });
+//   });
+// });
+
+// afterAll(async () => {
+//   try {
+//     await mongoose.disconnect();
+//   } catch (err) {
+//     throw err;
+//   }
+// });
